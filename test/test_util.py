e88d374941690d503aeb0af6381bbb9cf118b0e5
Rendy Kuo <maxgreg13625@gmail.com>
2021-10-24 19:58:42 +0800

    add get_golden_key_dict

--- test/test_util.py
+++ test/test_util.py
@@ -2,6 +2,13 @@ import pytest
 from ..util import *
 
 @pytest.mark.parametrize('seed', [32, 64])
-def test_get_random_key(seed):
+def test_get_random_key(seed: int):
 	key = get_random_key(seed)
-	assert seed is not None and type(key) == bytes
+	assert key is not None and type(key) == bytes
+
+@pytest.mark.parametrize('pwd', ['test'])
+def test_get_golden_key_dict(pwd: str):
+	key_dict = get_golden_key_dict(pwd)
+
+	assert key_dict is not None and type(key_dict) == dict
+	assert pwd in key_dict and len(key_dict[pwd]) == 2
