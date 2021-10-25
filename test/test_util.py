9153a8e68cfde99a0ab33a3d07e39070320c5a4a
Rendy Kuo <maxgreg13625@gmail.com>
2021-10-25 21:44:45 +0800

    add import/export bin

--- test/test_util.py
+++ test/test_util.py
@@ -21,3 +21,21 @@ def test_get_golden_key_dict(pwd: str):
 def test_config():
 	config = read_config()
 	assert 'KVA' in config
+	assert 'PWD' in config['KVA'] and 'BIN_PATH' in config['KVA']
+
+@pytest.fixture()
+def config():
+	config = read_config()
+	return config
+
+def test_salt_bin(config):
+	pwd = config['KVA']['PWD']
+	# get salt and golden key
+	key_dict = get_golden_key_dict(pwd)
+
+	export_bin(key_dict[pwd][0], 'salt_test')
+	salt_from_file = import_bin('salt_test')
+	# check salt is the same after land to file
+	assert key_dict[pwd][0] == salt_from_file
+	# check can get same golden key with salt read from file
+	assert key_dict[pwd][1] == get_golden_key(pwd, salt_from_file)
