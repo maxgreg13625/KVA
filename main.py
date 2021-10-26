fe206416cbf5a72bdaa51e8d9490375de64d91bd
Rendy Kuo <maxgreg13625@gmail.com>
2021-10-26 22:09:02 +0800

    update main function with kva_aes

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from util import *
+import logging
+
+logger = logging.getLogger(__name__)
 
 def main():
-	pass
+	kva_aes = KVA_AES()
+	logging.info(f'{kva_aes.iv}  {kva_aes.golden_key}')
 
 if __name__ == '__main__':
 	main()
